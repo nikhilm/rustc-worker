c1ac62bbe4c0b2e3734dfafbdcd4a97289ace529
Nikhil Marathe <nsm.nikhil@gmail.com>
2020-09-18 10:26:40 -0700

    fix the buffered reading to read the right amount of data from stdin for each request

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use bytes::Buf;
 use prost::Message;
 use std::io::BufRead;
 use std::io::Read;
@@ -17,13 +18,57 @@ fn main() -> std::io::Result<()> {
     // If started as a persistent worker.
     if let Some(arg) = args.peek() {
         if arg == "--persistent_worker" {
+            // TODO: Move this to a lib.rs.
+            let stdin = std::io::stdin();
+            let mut locked = stdin.lock();
+            // We need some kind of circular buffer that can be written to on one end by calling
+            // read, and can be read from on the other end in little bits, and then moved forward
+            // by the little bit we don't care about.
             loop {
-                // TODO: Move this to a lib.rs.
-                // TODO: Smarter buffer usage.
-                let mut buffer = [0; 10000];
-                eprintln!("READ {} BYTES", std::io::stdin().read(&mut buffer[..])?);
+                let mut len_buffer = vec![0; 10];
+                let mut offset = 0;
+                let msg_len = loop {
+                    let n = locked.read(&mut len_buffer[offset..])?;
+                    offset += n;
+                    eprintln!(
+                        "Read {} offset {} has {} {:?}",
+                        n,
+                        offset,
+                        len_buffer.len(),
+                        &len_buffer[..len_buffer.len()]
+                    );
+                    match prost::decode_length_delimiter(&len_buffer[..]) {
+                        Ok(n) => {
+                            eprintln!("Know how much to read {}", n);
+                            break n;
+                        }
+                        _ => {
+                            if offset >= len_buffer.len() {
+                                return Err(std::io::Error::new(
+                                    std::io::ErrorKind::Other,
+                                    "Invalid length",
+                                ));
+                            }
+                        }
+                    }
+                };
+                // Infer how much of the len buffer actually has data.
+                let encoded_len = prost::length_delimiter_len(msg_len);
+                eprintln!(
+                    "Encoded len {}, so need to grab rest of vec {:?}",
+                    encoded_len,
+                    &len_buffer[encoded_len..]
+                );
+                let mut data_buffer = vec![0; msg_len];
+                eprintln!("Created data bufer with len {}", data_buffer.len());
+                let already_read = len_buffer.len() - encoded_len;
+                // Copy over left over data after reading len.
+                (&mut data_buffer[0..already_read]).copy_from_slice(&len_buffer[encoded_len..]);
+                // Read the rest.
+                locked.read_exact(&mut data_buffer[already_read..])?;
+                eprintln!("Beginning of data buffer is {:?}", &data_buffer[..20]);
                 let message: worker_protocol::WorkRequest =
-                    prost::Message::decode_length_delimited(&buffer[..])?;
+                    prost::Message::decode(&data_buffer[..])?;
                 eprintln!("Req ID: {}", message.request_id);
                 eprintln!("Req args: {:?}", message.arguments);
                 eprintln!("---");
